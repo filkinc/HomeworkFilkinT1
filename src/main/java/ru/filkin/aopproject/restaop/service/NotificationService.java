0eec777bb2f635872415c993e295fbeb50c610e2
filkinc <filkin.daniel@yandex.ru>
2025-03-02 19:19:17 +0300

    add: sending by mail

--- src/main/java/ru/filkin/aopproject/restaop/service/NotificationService.java
+++ src/main/java/ru/filkin/aopproject/restaop/service/NotificationService.java
@@ -1,11 +1,34 @@
 package ru.filkin.aopproject.restaop.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class NotificationService {
 
+    private final JavaMailSender mailSender;
+
+    public NotificationService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     public void sendNotification(int taskId, String newStatus) {
-        System.out.println("Sending notification: Task ID=" + taskId + ", New Status=" + newStatus);
+        log.info("Sending notification: Task ID={}, New Status={}", taskId, newStatus);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("filkin_test_address@mail.ru");
+        message.setTo("filkin_test_address@mail.ru");
+        message.setSubject("Task Status Updated");
+        message.setText("Task ID: " + taskId + "\nNew Status: " + newStatus);
+
+        try {
+            mailSender.send(message);
+            log.info("Email sent successfully!");
+        } catch (Exception e) {
+            log.error("Failed to send email: Task ID={}, New Status={}", taskId, newStatus, e);
+        }
     }
 }
