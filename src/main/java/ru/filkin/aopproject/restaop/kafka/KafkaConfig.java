26449b517343d5c17a3fe5e4073e5f597c5859ef
filkinc <filkin.daniel@yandex.ru>
2025-03-04 14:50:18 +0300

    fixed KafkaConfig

--- src/main/java/ru/filkin/aopproject/restaop/kafka/KafkaConfig.java
+++ src/main/java/ru/filkin/aopproject/restaop/kafka/KafkaConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -21,9 +22,14 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    private static final String BOOTSTRAP_SERVERS = "localhost:29092";
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String BOOTSTRAP_SERVERS;
 
-    private static final String TOPIC = "tasks-topic";
+    @Value("${spring.kafka.topic.name}")
+    private String TOPIC;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String GROUP_ID;
 
     @Bean
     public NewTopic taskUpdatesTopic(){
@@ -50,8 +56,9 @@ public class KafkaConfig {
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "task-group");
-        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "ru.filkin.aopproject.restaop.kafka");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        configProps.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10);
         configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return new DefaultKafkaConsumerFactory<>(configProps, new StringDeserializer(), new JsonDeserializer<>(TaskUpdateEvent.class));
     }
