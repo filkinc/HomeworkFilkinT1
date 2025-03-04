2c900fce59e813c43dbd1b273f5ea17d4d98d563
filkinc <filkin.daniel@yandex.ru>
2025-03-04 15:01:53 +0300

    fixed LoggingAspect

--- src/main/java/ru/filkin/aopproject/restaop/aspect/LoggingAspect.java
+++ src/main/java/ru/filkin/aopproject/restaop/aspect/LoggingAspect.java
@@ -15,26 +15,34 @@ public class LoggingAspect {
     public void serviceMethods(){
     }
 
-    @Before("serviceMethods()")
+    @Pointcut("execution(* ru.filkin.aopproject.restaop.kafka.*.*(..))")
+    public void kafkaMethods(){
+    }
+
+    @Pointcut("serviceMethods() || kafkaMethods()")
+    public void allMethods() {
+    }
+
+    @Before("allMethods()")
     public void logBeforeMethodCall(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
         log.info("Service: Вызов метода {} с аргументами {}", methodName, args);
     }
 
-    @AfterReturning(value = "serviceMethods()", returning = "result")
+    @AfterReturning(value = "allMethods()", returning = "result")
     public void logAfterMethodCall(JoinPoint joinPoint, Object result){
         String methodName = joinPoint.getSignature().getName();
         log.info("Service: Метод {} выполнен, результат {}", methodName, result);
     }
 
-    @AfterThrowing(value = "serviceMethods()", throwing = "exception")
+    @AfterThrowing(value = "allMethods()", throwing = "exception")
     public void logMethodException(JoinPoint joinPoint, Exception exception){
         String methodName = joinPoint.getSignature().getName();
         log.error("Service: Метод {} выбросил исключение {}", methodName, exception.getMessage());
     }
 
-    @Around("serviceMethods()")
+    @Around("allMethods()")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().getName();
         Long startTime = System.currentTimeMillis();
