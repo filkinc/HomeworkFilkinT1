830236dd0f362dcc47e5197f80fe97bf0bad425a
filkinc <filkin.daniel@yandex.ru>
2025-03-04 14:53:17 +0300

    fixed Producer, Consumer

--- src/main/java/ru/filkin/aopproject/restaop/kafka/KafkaProducer.java
+++ src/main/java/ru/filkin/aopproject/restaop/kafka/KafkaProducer.java
@@ -1,6 +1,8 @@
 package ru.filkin.aopproject.restaop.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +12,15 @@ public class KafkaProducer {
 
     private final KafkaTemplate<String, TaskUpdateEvent> kafkaTemplate;
 
+    @Value("${spring.kafka.topic.name}")
+    private String TOPIC;
+
     public KafkaProducer(KafkaTemplate<String, TaskUpdateEvent> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendTaskUpdate(TaskUpdateEvent event) {
         log.info("Sending task update event: {}", event);
-        kafkaTemplate.send("task-topic", event);
+        kafkaTemplate.send(TOPIC, event);
     }
 }
