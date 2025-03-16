7ca258ea91c2dd7c1044c070a0acc651e73d2979
filkinc <filkin.daniel@yandex.ru>
2025-03-16 14:31:19 +0300

    fixed TaskControllerTest

--- src/test/java/ru/filkin/aopproject/restaop/controller/TaskControllerTest.java
+++ src/test/java/ru/filkin/aopproject/restaop/controller/TaskControllerTest.java
@@ -1,40 +1,67 @@
 package ru.filkin.aopproject.restaop.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.filkin.aopproject.restaop.PostgresContainer;
-import ru.filkin.aopproject.restaop.model.TaskDTO;
+import ru.filkin.aopproject.restaop.model.Task;
+import ru.filkin.aopproject.restaop.repository.TaskRepository;
 import ru.filkin.aopproject.restaop.service.TaskService;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(TaskController.class)
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration"
+})
+@AutoConfigureMockMvc
 public class TaskControllerTest extends PostgresContainer {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private TaskService taskService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        taskRepository.deleteAll();
+    }
 
     @Test
     public void whenGetTaskById_thenReturnTask() throws Exception {
 
-        TaskDTO taskDTO = new TaskDTO(1, "Test Task", "Description", 123,"In Progress");
-        when(taskService.getTaskById(1)).thenReturn(taskDTO);
+        String taskJson = "{\"title\": \"Test Task\", \"description\": \"Description\", \"userId\": 10, \"status\": \"In Progress\"}";
 
-        mockMvc.perform(get("/tasks/1"))
+        String response = mockMvc.perform(post("/tasks")
+                        .contentType("application/json")
+                        .content(taskJson))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Integer taskId = objectMapper.readTree(response).get("id").asInt();
+
+        mockMvc.perform(get("/tasks/{id}", taskId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(taskId))
                 .andExpect(jsonPath("$.title").value("Test Task"))
                 .andExpect(jsonPath("$.description").value("Description"))
-                .andExpect(jsonPath("$.userId").value(123))
+                .andExpect(jsonPath("$.userId").value(10))
                 .andExpect(jsonPath("$.status").value("In Progress"));
     }
+
 }
